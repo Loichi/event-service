39e5373c631dc8296ccc0a2d15676c28c3419954
unknown <loiccape.pro@outlook.fr>
2023-08-14 13:33:25 +0200

    update docker file

--- model/Event.js
+++ model/Event.js
@@ -8,8 +8,9 @@ const EventSchema = mongoose.Schema({
     name: {type: String, required: true},
     date: {type: Date, required: true},
     location : {type:String , required: false},
-    eventType : {type: String , required: false},
+    eventType : {type: String , required: true},
     guestCount : {type: Number, required : false},
+    menus: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Menu' , required: false}]
 });
 
 module.exports = mongoose.model('Event', EventSchema);
