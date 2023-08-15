42838f5912fc1756a60550b61bf71dc8f3d82a4e
unknown <loiccape.pro@outlook.fr>
2023-08-15 12:35:06 +0200

    model modif

--- model/Event.js
+++ model/Event.js
@@ -6,11 +6,15 @@ mongoose.plugin(muv);
 const EventSchema = mongoose.Schema({
     
     name: {type: String, required: true},
-    date: {type: Date, required: true},
     location : {type:String , required: false},
     eventType : {type: String , required: true},
+    clientName: {type: String, required: true},
     guestCount : {type: Number, required : false},
-    menus: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Menu' , required: false}]
+    menu: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Menu' , required: false}],
+    totalPrice : {type: Number, required : false},
+    date: {type: Date, required: true},
+    eventDone: {type: Boolean, required:true}
+
 });
 
 module.exports = mongoose.model('Event', EventSchema);
