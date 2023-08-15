ba5be632ce18141b4053d4a28e6aaddc1cdc0e05
unknown <loiccape.pro@outlook.fr>
2023-08-15 12:41:36 +0200

    controller modif

--- controller/EventController.js
+++ controller/EventController.js
@@ -3,21 +3,13 @@ const Event = require("../model/Event");
 //Create
 async function createEvent(req, res) {
   try {
-    const { name, date, location, eventType, guestCount } = req.body;
-
-    const newEvent = await Event.create({
-      name,
-      date,
-      location,
-      eventType,
-      guestCount,
-    });
+    const newEvent = await Event.create(req.body);
     res.status(201).json(newEvent);
   } catch (err) {
     console.log(err);
     res
       .status(500)
-      .json({ error: "An error occured white creating the event" });
+      .json({ error: "An error occured while creating the event" });
   }
 }
 
